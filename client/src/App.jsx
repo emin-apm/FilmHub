27882c4696b3260f6e6f79390e6672cf4da41d5b
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-04-01 14:55:55 +0300

    feat: add move details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./layouts/Footer/Footer";
 import Navbar from "./layouts/Navigation/Navbar";
 import HomePage from "./pages/HomePage";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <MoviePage />
       <Footer />
     </>
   );
