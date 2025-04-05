fdad092e58a107c094d4ead27b177cb67aab961b
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-04-05 18:12:57 +0300

    feat(MovieDetails): movie details 3th part sites added

--- client/src/components/MovieDetails/MovieDetails.jsx
+++ client/src/components/MovieDetails/MovieDetails.jsx
@@ -33,7 +33,73 @@ export default function MovieDetails() {
           <i className="fa-solid fa-circle-play"></i>
         </div>
       </div>
-      <div>asdasd</div>
+      <div className={styles.movieDetails}>
+        <h1>The Dark Knight</h1>
+        <p>
+          Batman has a new foe, the Joker, who is an accomplished criminal
+          hell-bent on decimating Gotham City. Together with Gordon and Harvey
+          Dent, Batman struggles to thwart the Joker before it is too late.
+        </p>
+      </div>
+      <div className={styles.movieSites}>
+        <div className={styles.siteContainer}>
+          <div className={styles.siteBox}>
+            <span>Filmi9 </span>
+            <span>
+              <img src="https://flagcdn.com/16x12/bg.png" alt="BG Flag" />
+              BG
+            </span>
+            <span>720p</span>
+            <a href="">Check out!</a>
+          </div>
+          <div className={styles.siteBox}>
+            <span>Vsi4kiFilmi </span>
+            <span>
+              <img src="https://flagcdn.com/16x12/bg.png" alt="BG Flag" />
+              BG
+            </span>
+
+            <span>720p</span>
+            <a href="">Check out!</a>
+          </div>
+          <div className={styles.siteBox}>
+            <span>Filmizip </span>
+            <span>
+              <img src="https://flagcdn.com/16x12/bg.png" alt="BG Flag" />
+              BG
+            </span>
+            <span>720p</span>
+            <a href="">Check out!</a>
+          </div>
+          <div className={styles.siteBox}>
+            <span>FullHDFilmIzlesene </span>
+            <span>
+              <img src="https://flagcdn.com/16x12/tr.png" alt="TR Flag" />
+              TR
+            </span>
+            <span>720p</span>
+            <a href="">Check out!</a>
+          </div>
+          <div className={styles.siteBox}>
+            <span>HDFilmCehennemi</span>
+            <span>
+              <img src="https://flagcdn.com/16x12/tr.png" alt="TR Flag" />
+              TR
+            </span>
+            <span>720p</span>
+            <a href="">Check out!</a>
+          </div>
+          <div className={styles.siteBox}>
+            <span>MovieStream</span>
+            <span>
+              <img src="https://flagcdn.com/16x12/gb.png" alt="GB Flag" />
+              EN
+            </span>
+            <span>720p</span>
+            <a href="">Check out!</a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
