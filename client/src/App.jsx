4ec4cf523adbc06dc0b8d479f944108ab96f1ced
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-04-15 12:33:50 +0300

    feat(app): create createBrowserRouter and add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
-import Footer from "./layouts/Footer/Footer";
-import Navbar from "./layouts/Navigation/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout/MainLayout";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "/movie/:id", element: <MoviePage /> },
+      ],
+    },
+  ]);
+
   return (
     <>
-      <Navbar />
-      {/* <HomePage /> */}
-      <MoviePage />
-      <Footer />
+      <RouterProvider router={router} />
     </>
   );
 }
