f0fe1c24d4d88f16a1039fcc1e37d56ceba7b98d
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-04-26 18:48:15 +0300

    feat:(HomePage) adding axios for api calls

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import Hero from "../components/Hero/Hero";
 import PopularMovies from "../components/PopularMovies/PopularMovies";
+import axios from "axios";
 
 export default function HomePage() {
   const {
@@ -10,14 +11,14 @@ export default function HomePage() {
   } = useQuery({
     queryKey: ["movies"],
     queryFn: async () => {
-      const res = await fetch("https://ghibliapi.vercel.app/films");
-      if (!res.ok) throw new Error("Network response was not ok");
-      return res.json();
+      const res = await axios.get("https://ghibliapi.vercel.app/films");
+      return res.data;
     },
   });
   console.log(movies);
 
   if (isLoading) return <div>Loading...</div>;
+  if (error instanceof Error) return <div>Error: {error.message}</div>;
 
   return (
     <>
