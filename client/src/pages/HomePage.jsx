1270801526f0d0a44bcb2b5d3b0725cd111d10e0
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-04-26 18:43:02 +0300

    feat:(HomePage):test with fake api for query

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import Hero from "../components/Hero/Hero";
 import PopularMovies from "../components/PopularMovies/PopularMovies";
 
 export default function HomePage() {
+  const {
+    data: movies,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["movies"],
+    queryFn: async () => {
+      const res = await fetch("https://ghibliapi.vercel.app/films");
+      if (!res.ok) throw new Error("Network response was not ok");
+      return res.json();
+    },
+  });
+  console.log(movies);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <>
       <Hero />
