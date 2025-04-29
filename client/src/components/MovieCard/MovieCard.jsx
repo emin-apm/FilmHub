ddc7124d4d0b8cef18cf27039e1b855faef4a935
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-04-29 13:55:18 +0300

    chore(movieCard):added fetched data

--- client/src/components/MovieCard/MovieCard.jsx
+++ client/src/components/MovieCard/MovieCard.jsx
@@ -1,27 +1,30 @@
 import { Link } from "react-router-dom";
 import styles from "./MovieCardStyles.module.css";
+import { formattedDate } from "../../utils/dateConvert";
 
-export default function MovieCard() {
+export default function MovieCard({ movie }) {
+  console.log(movie);
   return (
-    <Link className={styles.cardBox} to={"/movie/123"}>
+    <Link className={styles.cardBox} to={`/movie/${movie.id}`}>
       <div href="/movie/123123" className={styles.cardOverlay}>
         <i className="fa-solid fa-play"></i>
       </div>
 
       <div className={styles.mainSliderImg}>
         <img
-          src="https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_.jpg"
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt=""
         />
       </div>
       <div className={styles.mainSliderText}>
         <div className={styles.raitingBox}>
-          9.0 <span className={styles.raiting}>imdb</span>{" "}
+          {movie.vote_average.toFixed(1)}{" "}
+          <span className={styles.raiting}>imdb</span>{" "}
         </div>
         <div className={styles.bottonText}>
           <div className={styles.movieName}>
-            <span>July 25, 2008</span>
-            <strong>The Dark Knigh</strong>
+            <span>{formattedDate(movie.release_date)}</span>
+            <strong>{movie.title}</strong>
           </div>
           <div className={styles.category}>
             <span>Action, Epic, Superhero</span>
