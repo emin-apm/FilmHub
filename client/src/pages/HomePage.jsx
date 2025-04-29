a033acb5500b7c90a7ed7b2b2969796ff705d5eb
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-04-29 13:54:20 +0300

    feat(homePage): created fetch funcitons for movies

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,29 +3,81 @@ import Hero from "../components/Hero/Hero";
 import PopularMovies from "../components/PopularMovies/PopularMovies";
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
 export default function HomePage() {
   const {
-    data: movies,
-    isLoading,
-    error,
+    data: popularMovies,
+    isLoading: popularIsLoading,
+    error: popularError,
   } = useQuery({
-    queryKey: ["movies"],
+    queryKey: ["popularMovies"],
     queryFn: async () => {
-      const res = await axios.get("https://ghibliapi.vercel.app/films");
-      return res.data;
+      const res = await axios.get(`${BASE_URL}/movie/popular`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+      return res.data.results;
     },
   });
-  console.log(movies);
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error instanceof Error) return <div>Error: {error.message}</div>;
+  const {
+    data: topRated,
+    isLoading: topRatedIsLoading,
+    error: topRatedError,
+  } = useQuery({
+    queryKey: ["topRatedMovies"],
+    queryFn: async () => {
+      const res = await axios.get(`${BASE_URL}/movie/top_rated`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+      return res.data.results;
+    },
+  });
+
+  const {
+    data: latestMovies,
+    isLoading: latestIsLoading,
+    error: latestError,
+  } = useQuery({
+    queryKey: ["latestMovie"],
+    queryFn: async () => {
+      const res = await axios.get(`${BASE_URL}/movie/upcoming`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+      return res.data.results;
+    },
+  });
+
+  const {
+    data: nowPlaying,
+    isLoading: nowPlayingIsLoading,
+    error: nowPlayingError,
+  } = useQuery({
+    queryKey: ["nowPlayingMovies"],
+    queryFn: async () => {
+      const res = await axios.get(`${BASE_URL}/movie/now_playing`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+      return res.data.results;
+    },
+  });
 
   return (
     <>
       <Hero />
-      <PopularMovies />
-      <PopularMovies />
-      <PopularMovies />
+      <PopularMovies title="Popular Movies" movies={popularMovies} />
+      <PopularMovies title="Top rated of all times" movies={topRated} />
+      <PopularMovies title="Latest" movies={latestMovies} />
+      <PopularMovies title="Now Playing" movies={nowPlaying} />
     </>
   );
 }
