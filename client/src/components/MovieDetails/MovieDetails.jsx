c8025b985ba5ffe61d63728aa9ed85d4a45e5c3d
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-05-05 21:32:17 +0300

    refactoring(MovieDetails): remove useEffects and create separate hooks for fetching

--- client/src/components/MovieDetails/MovieDetails.jsx
+++ client/src/components/MovieDetails/MovieDetails.jsx
@@ -2,81 +2,16 @@ import styles from "./MovieDetailsStyles.module.css";
 import TrailerModal from "../TrailerModal/TrailerModal";
 import MovieSites from "./MovieSites";
 import Spiner from "../Spiner/Spiner";
-
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-
-import { useMovieDetails } from "../../utils/useMovieDetails";
+import { useState } from "react";
 import { formattedDate } from "../../utils/dateConvert";
 
-const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const OMBD_API_KEY = import.meta.env.VITE_OMBD_API_KEY;
-
-export default function MovieDetails() {
+export default function MovieDetails({ movie, trailerUrl, imdbRating }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [trailerUrl, setTrailerUrl] = useState(null);
-  const [imdbRating, setImdbRating] = useState(null);
-  const { id } = useParams();
 
-  const { data: movie, isLoading, error } = useMovieDetails(id);
-
-  useEffect(() => {
-    const fetchTrailer = async () => {
-      try {
-        const res = await axios.get(`${BASE_URL}/movie/${id}/videos`, {
-          params: {
-            api_key: API_KEY,
-          },
-        });
-        const trailers = res.data.results;
-        const youtubeTrailer = trailers.find(
-          (video) => video.site === "YouTube" && video.type === "Trailer"
-        );
-        if (youtubeTrailer) {
-          setTrailerUrl(
-            `https://www.youtube.com/embed/${youtubeTrailer.key}?autoplay=1`
-          );
-        }
-      } catch (error) {
-        console.error("Error fetching trailer:", error);
-      }
-    };
-
-    if (id) {
-      fetchTrailer();
-    }
-  }, [id]);
-
-  useEffect(() => {
-    const fetchImdbRaiting = async () => {
-      if (!movie?.imdb_id) return;
-
-      try {
-        const res = await axios.get(`https://www.omdbapi.com/`, {
-          params: {
-            apikey: OMBD_API_KEY,
-            i: movie.imdb_id,
-          },
-        });
-
-        if (res.data) {
-          setImdbRating(res.data);
-        }
-      } catch (error) {
-        console.error("Error fetching imdb score:", error);
-      }
-    };
-    fetchImdbRaiting();
-  }, [movie?.imdb_id]);
-
-  if (isLoading) {
-    return <Spiner />;
-  }
+  if (!movie) return <Spiner />;
 
   return (
-    <section className={`container`}>
+    <section className="container">
       <TrailerModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
@@ -94,7 +29,7 @@ export default function MovieDetails() {
             <h1>{movie.title}</h1>
             <div className={styles.moreAbout}>
               <div className={styles.raiting}>
-                <span>IMDB {imdbRating?.imdbRating}</span>
+                <span>IMDB {imdbRating?.imdbRating || "N/A"}</span>
               </div>
               <div className={styles.metaData}>
                 <span>{formattedDate(movie.release_date)}</span>
@@ -122,15 +57,11 @@ export default function MovieDetails() {
         </p>
         <p>
           <strong>Distributed by: </strong>
-          {movie.production_companies &&
-          movie.production_companies.length > 0 ? (
-            movie.production_companies.map((company) => (
+          {movie.production_companies?.length > 0 ? (
+            movie.production_companies.map((company, index) => (
               <span key={company.id}>
                 {company.name}
-                {company.id !==
-                  movie.production_companies[
-                    movie.production_companies.length - 1
-                  ].id && ", "}
+                {index < movie.production_companies.length - 1 && ", "}
               </span>
             ))
           ) : (
@@ -139,11 +70,10 @@ export default function MovieDetails() {
         </p>
         <p>
           <strong>IMDb: </strong>
-          {imdbRating?.imdbRating} ({imdbRating?.imdbVotes})
+          {imdbRating?.imdbRating || "N/A"} ({imdbRating?.imdbVotes || "N/A"})
         </p>
       </div>
 
-      {/* 3rd party movie sites */}
       <MovieSites title={movie.title} />
     </section>
   );
--- client/src/pages/MoviePage.jsx
+++ client/src/pages/MoviePage.jsx
@@ -1,5 +1,27 @@
 import MovieDetails from "../components/MovieDetails/MovieDetails";
+import { useParams } from "react-router-dom";
+
+import { useMovieDetails } from "../hooks/useMovieDetails";
+import { useMovieTrailer } from "../hooks/useMovieTrailer";
+import { useImdbRating } from "../hooks/useImdbRaiting";
 
 export default function MoviePage() {
-  return <MovieDetails />;
+  const { id } = useParams();
+
+  const { data: movie, isLoading, error } = useMovieDetails(id);
+  const { data: trailerUrl, isLoading: trailerLoading } = useMovieTrailer(id);
+  const { data: imdbRating, isLoading: ratingLoading } = useImdbRating(
+    movie?.imdb_id
+  );
+
+  if (isLoading || trailerLoading || ratingLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading movie details</p>;
+
+  return (
+    <MovieDetails
+      movie={movie}
+      trailerUrl={trailerUrl}
+      imdbRating={imdbRating}
+    />
+  );
 }
