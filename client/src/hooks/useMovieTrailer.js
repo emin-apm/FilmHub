995744317515bb20a1bc939376a63065d0116bd7
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-05-05 21:32:36 +0300

    feat: create movie trailer hook

---
+++ client/src/hooks/useMovieTrailer.js
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+export const useMovieTrailer = (id) => {
+  return useQuery({
+    queryKey: ["movieTrailer", id],
+    queryFn: async () => {
+      const res = await axios.get(`${BASE_URL}/movie/${id}/videos`, {
+        params: { api_key: API_KEY },
+      });
+
+      const trailers = res.data.results;
+      const youtubeTrailer = trailers.find(
+        (video) => video.site === "YouTube" && video.type === "Trailer"
+      );
+
+      return youtubeTrailer
+        ? `https://www.youtube.com/embed/${youtubeTrailer.key}?autoplay=1`
+        : null;
+    },
+    enabled: !!id,
+  });
+};
