3f9dfa507a06bf1b2ace9b8012ae10a9850bdf43
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-05-11 14:35:15 +0300

    feat: create catalog page with functions and css

---
+++ client/src/components/Catalog/Catalog.jsx
@@ -0,0 +1,40 @@
+import MovieCard from "../MovieCard/MovieCard";
+import styles from "./CatalogStyles.module.css";
+
+export default function Catalog({
+  title,
+  movies,
+  genres,
+  selectedGenres,
+  handleGenreToggle,
+}) {
+  return (
+    <section className={`${styles.catalogContainer} container`}>
+      <div className={styles.heading}>
+        <h2 className={styles.headingTitle}>{title}</h2>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        {Object.entries(genres).map(([id, name]) => (
+          <button
+            key={id}
+            onClick={() => handleGenreToggle(Number(id))}
+            className={
+              selectedGenres.includes(Number(id))
+                ? styles.selectedButton
+                : styles.button
+            }
+          >
+            {name}
+          </button>
+        ))}
+      </div>
+
+      <div className={styles.moviesContainer}>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
+}
