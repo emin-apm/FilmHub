c52fa28306d1b6436a41e49aa2aad39e18d5d2ce
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-05-11 14:36:22 +0300

    feat(MovieCard): add genres to the movie cards

--- client/src/components/MovieCard/MovieCard.jsx
+++ client/src/components/MovieCard/MovieCard.jsx
@@ -1,32 +1,40 @@
 import { Link } from "react-router-dom";
 import styles from "./MovieCardStyles.module.css";
 import { formattedDate } from "../../utils/dateConvert";
+import genres from "../../data/genre.json";
 
 export default function MovieCard({ movie }) {
+  const genreNames = movie.genre_ids
+    ?.map((id) => genres[id])
+    .filter(Boolean)
+    .join(", ");
+
   return (
     <Link className={styles.cardBox} to={`/movie/${movie.id}`}>
-      <div href="/movie/123123" className={styles.cardOverlay}>
+      <div className={styles.cardOverlay}>
         <i className="fa-solid fa-play"></i>
       </div>
 
       <div className={styles.mainSliderImg}>
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt=""
+          alt={movie.title}
         />
       </div>
+
       <div className={styles.mainSliderText}>
         <div className={styles.raitingBox}>
           {movie.vote_average.toFixed(1)}{" "}
-          <span className={styles.raiting}>imdb</span>{" "}
+          <span className={styles.raiting}>imdb</span>
         </div>
+
         <div className={styles.bottonText}>
           <div className={styles.movieName}>
             <span>{formattedDate(movie.release_date)}</span>
             <strong>{movie.title}</strong>
           </div>
           <div className={styles.category}>
-            <span>Action, Epic, Superhero</span>
+            <span>{genreNames || "No genres available"}</span>
           </div>
         </div>
       </div>
