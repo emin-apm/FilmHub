ef80286b0f9a4edf799fd3313c9fc00c3c7ef0f1
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-05-11 14:34:04 +0300

    feat(ExplorePage): add explore page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./layouts/MainLayout/MainLayout";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
 import NotFound404 from "./components/404/notFound404";
+import ExplorePage from "./pages/ExplorePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -13,6 +14,7 @@ function App() {
       children: [
         { index: true, element: <HomePage /> },
         { path: "/movie/:id", element: <MoviePage /> },
+        { path: "/explore", element: <ExplorePage /> },
       ],
     },
   ]);
