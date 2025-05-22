bb4bd2bd6c6520be904bac1852d24463535ae984
Emin Apturaim <eminapturaim@gmail.com>
2025-05-23 00:23:55 +0300

    feat(Pagination): add logic for first page,last page and overall pages

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,23 +1,65 @@
 import styles from "./Pagination.styles.module.css";
 
 export default function Pagination({ currentPage, totalPages, onPageChange }) {
+  const maxPage = Math.min(totalPages, 500); // TMDB max
+
   return (
-    <section>
-      <div className={styles.pagesContainer}>
-        <button
-          onClick={() => onPageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-        <h1 className={styles.pages}>{currentPage}</h1>
-        <button
-          onClick={() => onPageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </button>
-      </div>
-    </section>
+    <div className={styles.pagesContainer}>
+      {/* First Page */}
+      <button onClick={() => onPageChange(1)} disabled={currentPage === 1}>
+        {"<<"}
+      </button>
+
+      {/* Previous Pages and Current */}
+      {currentPage === 1 ? (
+        <>
+          <h2 className={styles.mainPage}>{currentPage}</h2>
+          {currentPage + 1 <= maxPage && (
+            <h2
+              className={styles.pages}
+              onClick={() => onPageChange(currentPage + 1)}
+            >
+              {currentPage + 1}
+            </h2>
+          )}
+          {currentPage + 2 <= maxPage && (
+            <h2
+              className={styles.pages}
+              onClick={() => onPageChange(currentPage + 2)}
+            >
+              {currentPage + 2}
+            </h2>
+          )}
+        </>
+      ) : (
+        <>
+          {currentPage - 1 > 0 && (
+            <h2
+              className={styles.pages}
+              onClick={() => onPageChange(currentPage - 1)}
+            >
+              {currentPage - 1}
+            </h2>
+          )}
+          <h2 className={styles.mainPage}>{currentPage}</h2>
+          {currentPage + 1 <= maxPage && (
+            <h2
+              className={styles.pages}
+              onClick={() => onPageChange(currentPage + 1)}
+            >
+              {currentPage + 1}
+            </h2>
+          )}
+        </>
+      )}
+
+      {/* Last Page */}
+      <button
+        onClick={() => onPageChange(maxPage)}
+        disabled={currentPage >= maxPage}
+      >
+        {">>"}
+      </button>
+    </div>
   );
 }
