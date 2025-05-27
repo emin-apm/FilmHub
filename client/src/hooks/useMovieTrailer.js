2bd496756d2a6dbcced737eca8f9a4e60ac5a9d7
Emin Apturaim <eminapturaim@gmail.com>
2025-05-27 12:22:03 +0300

    feat(useMovieTrailer): add media_type for fetching proper data

--- client/src/hooks/useMovieTrailer.js
+++ client/src/hooks/useMovieTrailer.js
@@ -1,26 +1,28 @@
+// hooks/useMovieTrailer.js
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-export const useMovieTrailer = (movieId) => {
+export function useMovieTrailer(id, media_type = "movie") {
   return useQuery({
-    queryKey: ["movieTrailer", movieId],
+    queryKey: ["movieTrailer", id, media_type],
     queryFn: async () => {
-      const res = await axios.get(`${BASE_URL}/movie/${movieId}/videos`, {
-        params: { api_key: API_KEY },
+      const res = await axios.get(`${BASE_URL}/${media_type}/${id}/videos`, {
+        params: {
+          api_key: API_KEY,
+          language: "en-US",
+        },
       });
 
-      const trailers = res.data.results;
-      const youtubeTrailer = trailers.find(
-        (video) => video.site === "YouTube" && video.type === "Trailer"
+      const trailer = res.data.results.find(
+        (video) =>
+          video.type === "Trailer" && video.site === "YouTube" && video.official
       );
 
-      return youtubeTrailer
-        ? `https://www.youtube.com/embed/${youtubeTrailer.key}?autoplay=1`
-        : null;
+      return trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : null;
     },
-    enabled: !!movieId,
+    enabled: !!id,
   });
-};
+}
