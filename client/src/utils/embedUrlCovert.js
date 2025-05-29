55b094534652addad79bc7ffdbf595d7a2035599
Emin Apturaim <eminapturaim@gmail.com>
2025-05-29 10:59:04 +0300

    fix(): add autoplay to modal util

--- client/src/utils/embedUrlCovert.js
+++ client/src/utils/embedUrlCovert.js
@@ -1,4 +1,4 @@
 export default function convertToEmbedUrl(url) {
   const match = url.match(/[?&]v=([^&]+)/);
-  return match ? `https://www.youtube.com/embed/${match[1]}` : null;
+  return match ? `https://www.youtube.com/embed/${match[1]}?autoplay=1` : null;
 }
