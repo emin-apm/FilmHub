c458e05e15226b347cb959be3fd30ff589829dc4
Emin Apturaim <eminapturaim@gmail.com>
2025-06-23 18:50:36 +0300

    feat(UserProfile):create UserProfile component

---
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import styles from "./UserProfileStyles.module.css";
+import porifilImg from "../../assets/profilImg.png";
+
+export default function UserProfile() {
+  const [imagePreview, setImagePreview] = useState(porifilImg);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImagePreview(imageUrl);
+    }
+  };
+
+  return (
+    <section className="container">
+      <div className={styles.profileContainer}>
+        <div className={styles.mediaContainer}>
+          <div className={styles.userImg}>
+            <img src={imagePreview} alt="Profile" />
+          </div>
+
+          <label className={styles.button}>
+            <i className="fa-solid fa-image"></i>
+            Change Photo
+            <input
+              type="file"
+              accept="image/*"
+              className={styles.fileInput}
+              onChange={handleImageChange}
+            />
+          </label>
+        </div>
+
+        <div className={styles.userInfo}>
+          <h1>eminapturaim@gmail.com</h1>
+          <h1>Eminkataa</h1>
+        </div>
+
+        <div className={styles.movieInfo}>
+          <p>Watchlater list: 3 movies</p>
+        </div>
+      </div>
+    </section>
+  );
+}
