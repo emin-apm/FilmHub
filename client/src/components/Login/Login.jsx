85c3c3e4ae5d6a7226e1a2e6649092b1c0eb3933
Emin Apturaim <eminapturaim@gmail.com>
2025-06-25 15:56:47 +0300

    feat(Login): create login component

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,56 @@
+import styles from "./LoginStyles.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.close}>
+        <i className="fa-solid fa-xmark"></i>
+      </span>
+      <div className={`${styles.formBox} ${styles.login}`}>
+        <h2>Login</h2>
+        <form action="">
+          <div className={styles.inputBox}>
+            <span className={styles.icon}>
+              <i className="fa-solid fa-envelope"></i>
+            </span>
+            <input type="email" placeholder=" " required />
+            <label htmlFor="">Email</label>
+          </div>
+          <div className={styles.inputBox}>
+            <span className={styles.icon}>
+              <i className="fa-solid fa-lock"></i>
+            </span>
+            <input type="password" placeholder=" " required />
+            <label htmlFor="">Password</label>
+          </div>
+          <div className={styles.rememberCheck}>
+            <label htmlFor="remember">
+              <input type="checkbox" id="remember" />
+              Remember Me
+            </label>
+            <a href="">Forgot password?</a>
+          </div>
+          <button type="submit" className={styles.btn}>
+            Login
+          </button>
+
+          <div className={styles.divider}>
+            <span className={styles.dividerText}>or</span>
+          </div>
+
+          <button type="submit" className={styles.btn}>
+            <i class="fa-brands fa-google"></i> Sign in with Google
+          </button>
+          <div className={styles.loginRegister}>
+            <p>
+              Don't have accaount?
+              <a href="" className={styles.registerLink}>
+                Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
