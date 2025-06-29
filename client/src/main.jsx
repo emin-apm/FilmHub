c07e78faab5e0339efbd06f133d15e256bd9dc3b
Emin Apturaim <eminapturaim@gmail.com>
2025-06-29 16:06:18 +0300

    feat(main): wrap project with context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { UserProvider } from "./context/UserContext.jsx";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
       <ReactQueryDevtools initialOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
