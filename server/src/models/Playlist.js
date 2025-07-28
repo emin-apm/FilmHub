45cde7e810655bfe4928c5258555d494ca44e15c
Emin Apturaim <eminapturaim@gmail.com>
2025-07-28 22:48:14 +0300

    feat(Playlist): add playlist model

---
+++ server/src/models/Playlist.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    movies: [
+      {
+        id: { type: String, required: true },
+        name: { type: String, required: true },
+        img: { type: String },
+        date: { type: String },
+        genre: [{ type: String }],
+      },
+    ],
+    sharedWith: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Playlist = mongoose.model("Playlist", playlistSchema);
+
+export default Playlist;
