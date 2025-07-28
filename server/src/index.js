0f21ebbedbad8ad26f6dbc9121bbf75d14996c88
Emin Apturaim <eminapturaim@gmail.com>
2025-07-28 22:37:42 +0300

    feat(logger): create and add logger for checks

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
+import { logger } from "./middleware/logger.js";
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+//test logger middleware
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello from server...!");
