33bfd8e54a02d7a59ed87d307baf4016ac1b1159
Emin Apturaim <eminapturaim@gmail.com>
2025-08-07 00:09:16 +0300

    feat(playlistController) add create playlist

--- server/src/controller/playlistController.js
+++ server/src/controller/playlistController.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import * as playlistService from "../services/playlistService.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Playlist data");
+router.post("/create", async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ message: "Playlist name is required" });
+    }
+
+    const playlist = await playlistService.createPlaylist(userId, name);
+
+    res.status(201).json(playlist);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 export default router;
