97880b88201aa6fec0ecf0e7749f48f3ee3a5803
Emin Apturaim <eminapturaim@gmail.com>
2025-08-15 12:45:44 +0300

    refactoring(userController): improving google sign in

--- server/src/controller/userController.js
+++ server/src/controller/userController.js
@@ -7,11 +7,8 @@ const router = Router();
 
 router.post("/register", toLowerCase, async (req, res) => {
   try {
-    const user = req.body;
-    const result = await userService.register(user);
-
+    const result = await userService.register(req.body);
     res.cookie("authcookie", result.refreshToken, cookieOptions);
-
     res.status(201).json(result.userData);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -20,9 +17,7 @@ router.post("/register", toLowerCase, async (req, res) => {
 
 router.post("/login", toLowerCase, async (req, res) => {
   try {
-    const user = req.body;
-    const result = await userService.login(user);
-
+    const result = await userService.login(req.body);
     res.cookie("authcookie", result.refreshToken, cookieOptions);
     res.status(200).json(result.userData);
   } catch (error) {
@@ -32,8 +27,12 @@ router.post("/login", toLowerCase, async (req, res) => {
 
 router.post("/googleSign", toLowerCase, async (req, res) => {
   try {
-    const user = req.body;
-    const result = await userService.googleSign(user);
+    const { access_token } = req.body;
+
+    if (!access_token) throw new Error("No token provided");
+
+    // Pass access_token to service
+    const result = await userService.googleSign({ access_token });
 
     res.cookie("authcookie", result.refreshToken, cookieOptions);
     res.status(200).json(result.userData);
@@ -45,12 +44,8 @@ router.post("/googleSign", toLowerCase, async (req, res) => {
 router.post("/refresh-token", async (req, res) => {
   try {
     const refreshToken = req.cookies.authcookie;
-    if (!refreshToken) {
-      throw new Error("Refresh token missing!");
-    }
-
+    if (!refreshToken) throw new Error("Refresh token missing!");
     const result = await userService.refreshToken(refreshToken);
-
     res.cookie("authcookie", result.refreshToken, cookieOptions);
     res.status(200).json({ accessToken: result.accessToken });
   } catch (error) {
@@ -59,13 +54,8 @@ router.post("/refresh-token", async (req, res) => {
 });
 
 router.post("/logout", (req, res) => {
-  res.clearCookie("authcookie", {
-    httpOnly: true,
-    secure: cookieOptions.secure,
-    sameSite: cookieOptions.sameSite,
-    path: cookieOptions.path,
-  });
-  res.status(200).json({ message: "Logged out succesfully" });
+  res.clearCookie("authcookie", cookieOptions);
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 export default router;
