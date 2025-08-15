dc80a3baa25dfdaa201d3ca982e1e91e64f89e52
Emin Apturaim <eminapturaim@gmail.com>
2025-08-15 12:47:23 +0300

    refactoring(tokenService): improvements

--- server/src/utils/tokenService.js
+++ server/src/utils/tokenService.js
@@ -1,11 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export function generateAccessToken(user) {
-  const payload = { _id: user._id, email: user.email };
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "10m" });
+  return jwt.sign(
+    { _id: user._id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
 }
 
 export function generateRefreshToken(user) {
-  const payload = { _id: user._id, email: user.email };
-  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+  return jwt.sign(
+    { _id: user._id, email: user.email },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: "7d" }
+  );
 }
