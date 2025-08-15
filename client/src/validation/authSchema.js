e9bb3d67252f1f401854b483bd3cc3bf0e979010
Emin Apturaim <eminapturaim@gmail.com>
2025-08-15 12:47:41 +0300

    feat(authSchema): add yup auth schema

---
+++ client/src/validation/authSchema.js
@@ -0,0 +1,16 @@
+import * as Yup from "yup";
+
+export const loginSchema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
+
+export const registerSchema = loginSchema.shape({
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Password must match")
+    .required("Confirm Password is required"),
+});
