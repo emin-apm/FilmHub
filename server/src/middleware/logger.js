b7673e721dd40546d7066aa863311a6cff835eb2
Emin Apturaim <eminapturaim@gmail.com>
2025-08-16 11:24:41 +0300

    feat(logger): add cookie checker

--- server/src/middleware/logger.js
+++ server/src/middleware/logger.js
@@ -1,4 +1,10 @@
 export const logger = (req, res, next) => {
   console.log(`Method:${req.method} \nURL:${req.url}`);
+
+  if (req.cookies?.authcookie) {
+    console.log("✅ Refresh token cookie is attached");
+  } else {
+    console.log("❌ No refresh token cookie found");
+  }
   next();
 };
