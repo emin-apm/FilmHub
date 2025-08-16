c856164de761746f8273621d68904d53f54ce5fd
Emin Apturaim <eminapturaim@gmail.com>
2025-08-16 11:26:23 +0300

    feat(UserContext): implement refresh-token check and persist user data

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,16 +1,47 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import porifilImg from "../assets/profilImg.png";
 
 let UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState({
-    picture: porifilImg,
-    // email: "emin@gmail.com",
+    avatar: porifilImg,
     email: null,
     username: null,
   });
   //Fetch logic for fetching user data...
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/user/refresh-token", {
+          method: "POST",
+          credentials: "include",
+        });
+
+        if (!res.ok) {
+          console.log("No valid refresh token or user not loged");
+          setUserData({
+            avatar: porifilImg,
+            email: null,
+            username: null,
+          });
+          return;
+        }
+
+        const { userData = {} } = await res.json();
+
+        setUserData({
+          avatar: userData.avatar || porifilImg,
+          email: userData.email || null,
+          username: userData.username || null,
+        });
+      } catch (err) {
+        console.error("Error checking auth:", err);
+      }
+    };
+    checkAuth();
+  }, []);
 
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
