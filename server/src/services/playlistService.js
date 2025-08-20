0a948b6fc886a9549ec5372c32d58c7ac904999e
Emin Apturaim <eminapturaim@gmail.com>
2025-08-20 12:27:25 +0300

    feat(playlistService): add, share, remove services

--- server/src/services/playlistService.js
+++ server/src/services/playlistService.js
@@ -5,7 +5,7 @@ export async function createPlaylist(userId, name) {
   // Create playlist with userId in sharedWith array
   const playlist = await Playlist.create({
     name,
-    createdBy: userId,
+    owner: userId,
     sharedWith: [userId],
   });
 
@@ -14,5 +14,52 @@ export async function createPlaylist(userId, name) {
     $push: { playlist: playlist._id },
   });
 
-  return await Playlist.findById(playlist._id).populate("createdBy");
+  return await Playlist.findById(playlist._id).populate(
+    "owner",
+    "username email avatar"
+  );
 }
+
+export async function addMovieToPlaylist(playlistId, userId, movie) {
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) throw new Error("Playlist not found");
+
+  // Permission check: user must be owner or in sharedWith
+  const isOwner = playlist.owner.equals(userId);
+  const isShared = playlist.sharedWith.some((id) => id.equals(userId));
+
+  if (!isOwner && !isShared) throw new Error("No permission to add movie");
+
+  playlist.movies.push(movie);
+  await playlist.save();
+
+  return playlist;
+}
+
+export async function sharePlaylistWithUser(
+  playlistId,
+  currentUserId,
+  userIdToAdd
+) {
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) throw new Error("Playlist not found");
+
+  // Permission check: only owner or shared user can share
+  const isOwner = playlist.owner.equals(currentUserId);
+  const isShared = playlist.sharedWith.some((id) => id.equals(currentUserId));
+
+  if (!isOwner && !isShared) {
+    throw new Error("You do not have permission to share this playlist");
+  }
+
+  // Add new user if not already in sharedWith
+  if (!playlist.sharedWith.includes(userIdToAdd)) {
+    playlist.sharedWith.push(userIdToAdd);
+    await playlist.save();
+  }
+
+  return await playlist.populate("sharedWith", "username email avatar");
+}
+
+//user movie service
