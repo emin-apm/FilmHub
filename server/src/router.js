4735c0e2fa68b1609a9d2dc972d4392e1eaf3f0d
Emin Apturaim <eminapturaim@gmail.com>
2025-08-20 12:24:26 +0300

    chore: name change from user to auth

--- server/src/router.js
+++ server/src/router.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import userController from "./controller/userController.js";
+import authController from "./controller/authController.js";
 import playlistController from "./controller/playlistController.js";
 
 const router = Router();
 
-router.use("/user", userController);
+router.use("/user", authController);
 router.use("/data/playlist", playlistController);
 
 export default router;
