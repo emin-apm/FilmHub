0d015f8947e80f68b6d53940d80a48f3165b8451
Emin Apturaim <eminapturaim@gmail.com>
2025-08-24 22:47:44 +0300

    feat(router): create separate path for user playlist

--- server/src/router.js
+++ server/src/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authController from "./controller/authController.js";
 import playlistController from "./controller/playlistController.js";
+import userPlaylistController from "./controller/userPlaylistController.js";
 
 const router = Router();
 
 router.use("/user", authController);
 router.use("/data/playlist", playlistController);
+router.use("/data/userPlaylist", userPlaylistController);
 
 export default router;
