786a0d5a948ff15bf3e8604b51f0fb3f99646d21
Emin Apturaim <eminapturaim@gmail.com>
2025-09-17 21:31:44 +0300

    perf(App): add lazy loading to improve performance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,38 @@
+import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout/MainLayout";
-import HomePage from "./pages/HomePage";
-import MoviePage from "./pages/MoviePage";
-import NotFound404 from "./components/404/notFound404";
-import ExplorePage from "./pages/ExplorePage";
-import TrendingPage from "./pages/TrendingPage";
-import TVShowsPage from "./pages/TVShowsPage";
-import WatchLaterPage from "./pages/WatchLaterPage";
-import ProfilPage from "./pages/ProfilPage";
+import NotFoundPage from "./components/404/notFound404";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
+const ExplorePage = lazy(() => import("./pages/ExplorePage"));
+const TrendingPage = lazy(() => import("./pages/TrendingPage"));
+const TVShowsPage = lazy(() => import("./pages/TVShowsPage"));
+const WatchLaterPage = lazy(() => import("./pages/WatchLaterPage"));
+const ProfilePage = lazy(() => import("./pages/ProfilPage"));
 
 function App() {
-  {
-    /* TO DO: create ROUTES   */
-  }
-
   const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
-      errorElement: <NotFound404 />,
+      errorElement: <NotFoundPage />,
       children: [
         { index: true, element: <HomePage /> },
-        { path: "/:media_type/:id", element: <MoviePage /> },
-        { path: "/explore", element: <ExplorePage /> },
-        { path: "/trending", element: <TrendingPage /> },
-        { path: "/tvshows", element: <TVShowsPage /> },
-        { path: "/watch-later", element: <WatchLaterPage /> },
-        { path: "/profile", element: <ProfilPage /> },
+        { path: ":media_type/:id", element: <MoviePage /> },
+        { path: "explore", element: <ExplorePage /> },
+        { path: "trending", element: <TrendingPage /> },
+        { path: "tvshows", element: <TVShowsPage /> },
+        { path: "watch-later", element: <WatchLaterPage /> },
+        { path: "profile", element: <ProfilePage /> },
       ],
     },
   ]);
 
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <RouterProvider router={router} />
-    </>
+    </Suspense>
   );
 }
 
