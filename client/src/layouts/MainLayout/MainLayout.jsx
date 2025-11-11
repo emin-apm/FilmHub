afaf8d5ee2dd3d04c60e8a5cd80df96698998a01
Emin Apturaim <eminapturaim@gmail.com>
2025-11-11 23:49:44 +0200

    perf(MainLayout): lazy-load Snowfall component

--- client/src/layouts/MainLayout/MainLayout.jsx
+++ client/src/layouts/MainLayout/MainLayout.jsx
@@ -2,17 +2,30 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navigation/Navbar";
 import ScrollToTop from "../../utils/useScrollToTop";
-import Snowfall from "../../components/Christmas/Snow/Snowfall";
-import Halloween from "../../components/Christmas/Halloween/Halloween";
+import React, { Suspense } from "react";
+
+const Snowfall = React.lazy(() =>
+  import("../../components/Christmas/Snow/Snowfall")
+);
+const Halloween = React.lazy(() =>
+  import("../../components/Christmas/Halloween/Halloween")
+);
 
 export default function MainLayout() {
   return (
     <>
       <ScrollToTop />
       <Navbar />
-      {/* <Snowfall /> */}
-      <Halloween />
-      <Outlet />
+
+      <Suspense fallback={null}>
+        <Snowfall />
+        {/* <Halloween /> */}
+      </Suspense>
+
+      <main>
+        <Outlet />
+      </main>
+
       <Footer />
     </>
   );
