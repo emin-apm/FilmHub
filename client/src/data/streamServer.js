bb738a55cdab07a6613f658b8b64d407cdd3bae9
Emin Apturaim <eminapturaim@gmail.com>
2025-12-18 14:45:00 +0200

    chore(streamServer): adjust servers

--- client/src/data/streamServer.js
+++ client/src/data/streamServer.js
@@ -12,26 +12,18 @@ export const streamServers = {
     movie: (id) => `https://player.videasy.net/movie/${id}`,
     tv: (id, s, e) => `https://player.videasy.net/tv/${id}/${s}/${e}`,
   },
-  vidlink: {
-    movie: (id) => `https://vidlink.pro/movie/${id}`,
-    tv: (id, s, e) => `https://vidlink.pro/tv/${id}/${s}/${e}`,
-  },
-  mapple: {
-    movie: (id) => `https://mapple.uk/watch/movie/${id}`,
-    tv: (id, s, e) => `https://mapple.uk/watch/tv/${id}/${s}/${e}`,
-  },
-  embedmaster: {
-    movie: (id) => `https://embedmaster.link/movie/${id}`,
-    tv: (id, s, e) => `https://embedmaster.link/tv/${id}/${s}/${e}`,
-  },
+  // vidlink: {
+  //   movie: (id) => `https://vidlink.pro/movie/${id}`,
+  //   tv: (id, s, e) => `https://vidlink.pro/tv/${id}/${s}/${e}`,
+  // },
+
   _111movies: {
     movie: (id) => `https://111movies.com/movie/${id}`,
     tv: (id, s, e) => `https://111movies.com/tv/${id}/${s}/${e}`,
   },
-  vidsrc: {
-    movie: (id) => `https://vidsrc.xyz/embed/movie/${id}`,
-    tv: (id, s, e) =>
-      `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=${s}&episode=${e}`,
+  mapple: {
+    movie: (id) => `https://mapple.uk/watch/movie/${id}`,
+    tv: (id, s, e) => `https://mapple.uk/watch/tv/${id}/${s}/${e}`,
   },
   fmovies4u: {
     movie: (id) => `https://fmovies4u.com/embed/tmdb-movie-${id}`,
@@ -39,10 +31,19 @@ export const streamServers = {
   },
   vidify: {
     movie: (id) => `https://player.vidify.top/embed/movie/${id}`,
-    tv: (id, s, e) => `/tv/${id}/${s}/${e}`,
+    tv: (id, s, e) => `https://player.vidify.top/embed/tv/${id}/${s}/${e}`,
   },
   vidfast: {
     movie: (id) => `https://vidfast.pro/movie/${id}`,
     tv: (id, s, e) => `https://vidfast.pro/tv/${id}/${s}/${e}`,
+  },
+  embedmaster: {
+    movie: (id) => `https://embedmaster.link/movie/${id}`,
+    tv: (id, s, e) => `https://embedmaster.link/tv/${id}/${s}/${e}`,
+  },
+  vidsrc: {
+    movie: (id) => `https://vidsrc.xyz/embed/movie/${id}`,
+    tv: (id, s, e) =>
+      `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=${s}&episode=${e}`,
   },
 };
